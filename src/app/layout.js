f22e5d31feaa605e874ad5bf6a5841237dd97440
peppe2412 <peppeterrazzano79@gmail.com>
2025-12-07 17:38:38 +0100

    Progetto creato / Scaffolding

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="black">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
