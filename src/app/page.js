71a6befb0ad65855865d3dcc201e6e3c3dff10f4
peppe2412 <peppeterrazzano79@gmail.com>
2025-12-20 15:27:55 +0100

    Aggiunto effetto canvas coriandoli

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import Confetti from "react-confetti-boom";
 import Header from "./sections/header/Header";
 import DescriptionChamp from "./sections/descriptionChamp/DescriptionChamp";
 import PilotDetails from "./sections/details/pilotDetails/PilotDetails";
@@ -7,6 +8,14 @@ import PilotDetails from "./sections/details/pilotDetails/PilotDetails";
 export default function Home() {
   return (
     <>
+      <Confetti
+        mode="boom"
+        particleCount={1000}
+        shapeSize={15}
+        spreadDeg={100}
+        colors={["#ff8000", "#ffd700"]}
+        style={{position: "fixed"}}
+      />
       <Header />
 
       <section className="sections-container">
